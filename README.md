fcee38f90fec376b956ad46d60d99a321338c77b
Lucas Corrêa <lucas.nhl@gmail.com>
2022-12-24 19:04:18 -0300

    🎉 initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Gestão Financeira
+
+Sistema de gestão financeira de clínicas de pilates/fisioterapia.
+Integração com o banco e com o sistema GINFES para emissão de NFS-e.
+
+### Features
+
+- [ ] - Controle de pacientes internos
+- [ ] - Exportar para .csv
+- [ ] - Integração Banco Inter (Extrato)
+- [ ] - Integração Banco Inter (Boletos)
+- [ ] - Integração GINFES (Consultar NFS-e)
+- [ ] - Integração GINFES (Emitir NFS-e)
+- [ ] - Integração GINFES (Cancelar NFS-e)
+- [ ] - Relatórios
+- [ ] - Alertas por e-mail
+
+## Nuxt 3 Minimal Starter
+
+Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
+
+## Setup
+
+Make sure to install the dependencies:
+
+```bash
+# yarn
+yarn install
+```
+
+## Development Server
+
+Start the development server on http://localhost:3000
+
+```bash
+yarn dev
+```
+
+## Production
+
+Build the application for production:
+
+```bash
+yarn build
+```
+
+Locally preview production build:
+
+```bash
+yarn preview
+```
+
+Check out the [deployment documentation](https://nuxt.com/docs/getting-started/deployment) for more information.
---
+++ nuxt.config.ts
@@ -0,0 +1,76 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    ['@nuxtjs/robots',
+      {
+        UserAgent: '*',
+        Disallow: ''
+      }
+    ],
+    '@nuxtjs/color-mode',
+    '@nuxtjs/html-validator',
+    'nuxt-icon',
+  ],
+
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: '123',
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: '/api'
+    }
+  },
+
+  typescript: {
+    shim: false
+  },
+
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    injectPosition: 0,
+    viewer: true,
+  },
+
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '',
+    storageKey: 'nuxt-color-mode'
+  },
+
+  htmlValidator: {
+    usePrettier: false,
+    logLevel: 'verbose',
+    failOnError: false,
+    options: {
+      extends: [
+        'html-validate:document',
+        'html-validate:recommended',
+        'html-validate:standard'
+      ],
+      rules: {
+        'svg-focusable': 'off',
+        'no-unknown-elements': 'error',
+        // Conflicts or not needed as we use prettier formatting
+        'void-style': 'off',
+        'no-trailing-whitespace': 'off',
+        // Conflict with Nuxt defaults
+        'require-sri': 'off',
+        'attribute-boolean-style': 'off',
+        'doctype-style': 'off',
+        // Unreasonable rule
+        'no-inline-style': 'off'
+      }
+    }
+  },
+
+
+})
