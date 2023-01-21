6a286d252b4c73342f1d0e9c97a2e09f017c8065
Lucas Corrêa <lucas.nhl@gmail.com>
2023-01-21 13:33:11 -0300

    🔨 change get token return object

--- plugins/interBank/token.ts
+++ plugins/interBank/token.ts
@@ -1,9 +1,11 @@
 // create a post request to get oauth token using defineEventHandler nuxt
 import { tokenPath } from './routes';
-import { HttpRequest } from '~/plugins/http';
+import { HttpRequest } from '@/plugins/http';
 import { ScopeType } from './scopeType';
-import { HttpResponseStatusCode } from '~/plugins/http/types';
+import { HttpResponseStatusCode } from '@/plugins/http/types';
 import handleErrors from '../errors';
+import { INTER_BANK_CLIENT_ID, INTER_BANK_CLIENT_SECRET } from '@/utils/config';
+import FormData from 'form-data';
 
 interface ITokenParams {
   client_id: string;
@@ -12,21 +14,37 @@ interface ITokenParams {
   scope: ScopeType;
 }
 
-interface ITokenResponse {
+export interface ITokenResponse {
   access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: ScopeType;
 }
 
-export default async (scope: ScopeType): Promise<string | never> => {
+export default async (scope: ScopeType): Promise<ITokenResponse | never> => {
   const data: ITokenParams = {
-    client_id: 'client_id',
-    client_secret: 'client',
+    client_id: INTER_BANK_CLIENT_ID,
+    client_secret: INTER_BANK_CLIENT_SECRET,
     grant_type: 'client_credentials',
     scope: scope
   };
-  const response = await HttpRequest.post<ITokenResponse>(tokenPath, data);
+  const formData = new FormData();
+  formData.append('client_id', data.client_id);
+  formData.append('client_secret', data.client_secret);
+  formData.append('grant_type', data.grant_type);
+  formData.append('scope', data.scope.toString());
+
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    ...formData.getHeaders()
+  };
+
+  const response = await HttpRequest.post<ITokenResponse>(tokenPath, formData, headers, true);
+
   if (response.statusCode === HttpResponseStatusCode.OK) {
-    return response.data.access_token;
+    return response.data;
   }
+
   throw handleErrors(response.statusCode, response.message ?? 'Error getting token');
 };
 
